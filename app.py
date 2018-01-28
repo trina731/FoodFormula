7401799500c714e9d0f46f82ec3aee10def25b77
trina731 <trinac0731@gmail.com>
2018-01-28 01:42:13 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import requests
 import sys
 from flask import Flask, request, render_template
 app = Flask(__name__)
+items=[]
 
 
 
-def createList(items):
+def createList():
     length = input("How many items are in your fridge? ")
     i = 0
     while i < length:
@@ -14,31 +15,28 @@ def createList(items):
 
 @app.route('/', methods=['GET'])
 
-def searchList(items):
-    list = []
+def searchList():
     str = ",".join(items)
-    print(str)
     recipe_list = []
     app_id = "ae8f41f7"
     app_key = "f6a79be567cb14862a2e7a4f3b4bf17c"
-
     auth = {'app_id': app_id,
-               'app_key': app_key}
-    list=auth
-    r = requests.get("https://api.edamam.com/search?q=" + str, list)
+               'app_key': app_key, 'q': str}
+    r = requests.get("https://api.edamam.com/search", params=auth)
     status = r.json()
     recipes = status["hits"]
     for recipe in recipes:
-       recipe_list.append({"name": recipe["recipe"]["label"], "ingredients": recipe["recipe"]["ingredientLines"],
-        "url": recipe["recipe"]["url"]})
+        name = recipe["recipe"]["label"]
+        url = recipe["recipe"]["url"]
+        #ingredients = recipe["recipe"]["ingredientLines"]
+        recipe_list.append(name)
+    recipeStr = ', '.join(recipe_list)
+    return recipeStr
 
-
-    print recipe_list
-
-list = []
-createList(list)
-(searchList(list))
+createList()
+searchList()
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(host='0.0.0.0')
+    app.run()
+
