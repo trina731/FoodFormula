260f50639ab0eab927ed201ea6430c20f6080091
trina731 <trinac0731@gmail.com>
2018-01-27 23:05:02 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+import requests
+import sys
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+
+
+def createList(items):
+    length = input("How many items are in your fridge? ")
+    print length
+    i = 0
+    while i < length:
+        items.append(raw_input("Enter item: "))
+        i = i + 1
+
+@app.route('/<string:FoodFormulaSearch>/', methods=['GET'])
+def render_static(FoodFormulaSearch):
+    return render_template('%s.html' % FoodFormulaSearch)
+
+if __name__ == "__main__":
+    app.run()
+    
+def searchList(items):
+    list = []
+    str = ",".join(items)
+    print(str)
+    recipe_list = []
+    app_id = "ae8f41f7"
+    app_key = "f6a79be567cb14862a2e7a4f3b4bf17c"
+
+    auth = {'app_id': app_id,
+               'app_key': app_key}
+    list=auth
+    r = requests.get("https://api.edamam.com/search?q=" + str, list)
+    status = r.json()
+    recipes = status["hits"]
+    for recipe in recipes:
+       recipe_list.append({"name": recipe["recipe"]["label"], "ingredients": recipe["recipe"]["ingredientLines"],
+        "url": recipe["recipe"]["url"]})
+
+
+    print recipe_list
+
+list = []
+createList(list)
+print(searchList(list))
